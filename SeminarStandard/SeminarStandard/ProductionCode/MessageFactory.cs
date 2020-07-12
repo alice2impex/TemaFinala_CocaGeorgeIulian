4ffdc6abdf5dfcdc2b81d67592d769b476495cd7
marianExceEdu <marianexceedu@gmail.com>
2020-07-12 21:36:26 +0300

    Refactoring 9.

--- SeminarStandard/SeminarStandard/ProductionCode/MessageFactory.cs
+++ SeminarStandard/SeminarStandard/ProductionCode/MessageFactory.cs
@@ -8,11 +8,12 @@ namespace SeminarStandard
 		{
 			if (!string.IsNullOrEmpty(name))
 			{
-
 				if (name != name.ToUpper())
 					return new SimpleMessage();
 				else
 					return new ShoutingMessage();
+
+				//ADD Royal Message detected by containing: "Prince or Princess, King, Queen"
 			}
 			return null;
 		}
