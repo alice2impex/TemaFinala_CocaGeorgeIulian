608524234c3c77afa3909c7a5d70211a53658320
alice2impex <Laptop@DESKTOP-SCEKIFJ>
2020-08-06 17:32:34 +0300

    TemaFin

--- SeminarStandard/SeminarStandard/Production/ArabicToRomanConverter.cs
+++ SeminarStandard/SeminarStandard/Production/ArabicToRomanConverter.cs
@@ -1,19 +1,45 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace SeminarStandard.Production
 {
 	internal class ArabicToRomanConverter
 	{
 		internal string Convert(int number)
-		{
-			StringBuilder result = new StringBuilder();
+        {
+                var retVal = new StringBuilder();
+                var valueMap = new SortedDictionary<int, string>
+                               {
+                                   { 1, "I" },
+                                   { 4, "IV" },
+                                   { 5, "V" },
+                                   { 9, "IX" },
+                                   { 10, "X" },
+                                   { 40, "XL" },
+                                   { 50, "L" },
+                                   { 90, "XC" },
+                                   { 100, "C" },
+                                   { 400, "CD" },
+                                   { 500, "D" },
+                                   { 900, "CM" },
+                                   { 1000, "M" },
+                               };
+                        if (number == 0)
+                        {
+                            return "Nu exista cifra 0";
+                        }
+                       
+                foreach (var kvp in valueMap.Reverse())
+                {
+                    while (number >= kvp.Key)
+                    {
+                        number -= kvp.Key;
+                        retVal.Append(kvp.Value);
+                    }
+                }
 
-			for (int i = 0; i < number; i++)
-			{
-				result.Append("I");
-			}
-
-			return result.ToString();
-		}
+                return retVal.ToString();
+            }
+        }
 	}
-}
--- SeminarStandard/SeminarStandard/TestUnits/ArabicToRomanConverterTests.cs
+++ SeminarStandard/SeminarStandard/TestUnits/ArabicToRomanConverterTests.cs
@@ -43,6 +43,58 @@ namespace SeminarStandard.TestUnits
 			ExecuteTest(4, "IV");
 		}
 
+		[Test]
+		public void Should_Return_V_For_5()
+		{
+			ExecuteTest(5, "V");
+		}
 
+		[Test]
+		public void Should_Return_XXII_For_22()
+		{
+			ExecuteTest(22, "XXII");
+		}
+
+		[Test]
+		public void Should_Return_XLV_For_45()
+		{
+			ExecuteTest(45, "XLV");
+		}
+
+		[Test]
+		public void Should_Return_XCIX_For_99()
+		{
+			ExecuteTest(99, "XCIX");
+		}
+
+		[Test]
+		public void Should_Return_DCC_For_700()
+		{
+			ExecuteTest(700, "DCC");
+		}
+
+		[Test]
+		public void Should_Return_MCM_For_1900()
+		{
+			ExecuteTest(1900, "MCM");
+		}
+
+		[Test]
+		public void Should_Return_MCMLXXXVI_For_1986()
+		{
+			ExecuteTest(1986, "MCMLXXXVI");
+		}
+
+		[Test]
+		public void Convert0_Error()
+		{
+			ExecuteTest(0, "Nu exista cifra 0");
+		}
+
+		[Test]
+		public void Should_Return_MMCCCXXI_For_2321()
+		{
+			ExecuteTest(2321, "MMCCCXXI");
+		}
 	}
 }
